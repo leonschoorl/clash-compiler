2e2e660b4a4e08cd8b9830fe39df1943335a8685
Leon Schoorl <leon@qbaylogic.com>
2019-01-09 17:10:31 +0100

    Make shift rotate test work for Int8 and Word8

--- tests/shouldwork/Numbers/ShiftRotate.hs
+++ tests/shouldwork/Numbers/ShiftRotate.hs
@@ -6,14 +6,16 @@ import Data.Int
 
 pat = 0b10011000
 
-inputs = $(listToVecTH [(0::Int)..16])
+type Shifts = 17
+inputs :: Vec Shifts Int
+inputs = iterateI succ 0
 
 testall v i
   = ( testAs @(Unsigned 8)  v i shiftsAndRots
     , testAs @(Signed 8)    v i shiftsAndRots
     , testAs @(BitVector 8) v i shiftsAndRots
-    -- , testAs @(Word8)       v i shiftsAndRots
-    -- , testAs @(Int8)        v i shiftsAndRots
+    , testAs @(Word8)       v i shiftsAndRots
+    , testAs @(Int8)        v i shiftsAndRots
     )
 {-# NOINLINE testall #-}
 
@@ -21,5 +23,7 @@ testAs
   :: Num b => Integer -> Int -> Vec n (b -> Int -> b) -> Vec n b
 testAs v i = map (\f -> f (fromInteger v) i)
 
-shiftsAndRots :: Bits a => Vec 4 (a -> Int -> a)
+type Ops = 4
+
+shiftsAndRots :: Bits a => Vec Ops (a -> Int -> a)
 shiftsAndRots = shiftL :> shiftR :> rotateL :> rotateR :> Nil
--- tests/shouldwork/Numbers/ShiftRotateTB.hs
+++ tests/shouldwork/Numbers/ShiftRotateTB.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE PartialTypeSignatures #-}
 module ShiftRotateTB where
 import Clash.Prelude
 import Clash.Explicit.Testbench
+import Data.Int
+import Data.Word
 import ShiftRotate
 
+expected :: Vec Shifts ( Vec Ops (Unsigned 8)
+                       , Vec Ops (Signed 8)
+                       , Vec Ops (BitVector 8)
+                       , Vec Ops Word8
+                       , Vec Ops Int8
+                       )
 expected = $(lift $ map (testall pat) inputs)
 
 topEntity = testall
@@ -10,7 +19,7 @@ topEntity = testall
 testBench :: Signal System Bool
 testBench = done
   where
-    testInput      = stimuliGenerator clk rst $ inputs
+    testInput      = stimuliGenerator clk rst inputs
     expectedOutput = outputVerifier clk rst expected
     done           = expectedOutput ((liftA2 topEntity) (pure pat) testInput)
     clk            = tbSystemClockGen (not <$> done)
