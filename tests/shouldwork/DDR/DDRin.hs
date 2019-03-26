01384dfbe9fdb5dc2e06325fcf07a10fbfff5d56
Leon Schoorl <leon@qbaylogic.com>
2019-03-26 15:38:30 +0100

    DDR tests: use BitVectors
    Because they're also compatible with the xilinx/intel primitives

--- tests/shouldwork/DDR/DDRin.hs
+++ tests/shouldwork/DDR/DDRin.hs
@@ -4,6 +4,8 @@ import Clash.Explicit.Testbench (ignoreFor)
 import Clash.Prelude (mux)
 import Clash.Explicit.Prelude
 import Clash.Explicit.DDR
+import Clash.Intel.DDR
+import Clash.Xilinx.DDR
 
 type DomReal = Dom "A" 2000 -- real clock domain
 type DomDDR  = Dom "A" 1000 -- fake doublespeed domain, used to model a ddr signal
@@ -16,48 +18,48 @@ The four variants defined here are all the combinations of
 
 topEntityGeneric :: Clock DomReal gated
           -> Reset DomReal synchronous
-          -> Signal DomDDR (Unsigned 8)
-          -> Signal DomReal (Unsigned 8, Unsigned 8)
+          -> Signal DomDDR (BitVector 8)
+          -> Signal DomReal (BitVector 8, BitVector 8)
 topEntityGeneric clk rst = ddrIn clk rst (i0,i1,i2)
--- topEntityGeneric clk rst = xilinxIddr clk rst
+-- topEntityGeneric clk rst = iddr clk rst
 -- topEntityGeneric clk rst = altddioIn (SSymbol @"Cyclone IV GX") clk rst
 
 
 topEntityUA :: Clock DomReal Source
           -> Reset DomReal Asynchronous
-          -> Signal DomDDR (Unsigned 8)
-          -> Signal DomReal (Unsigned 8, Unsigned 8)
+          -> Signal DomDDR (BitVector 8)
+          -> Signal DomReal (BitVector 8, BitVector 8)
 topEntityUA = topEntityGeneric
 
 topEntityUS :: Clock DomReal Source
           -> Reset DomReal Synchronous
-          -> Signal DomDDR (Unsigned 8)
-          -> Signal DomReal (Unsigned 8, Unsigned 8)
+          -> Signal DomDDR (BitVector 8)
+          -> Signal DomReal (BitVector 8, BitVector 8)
 topEntityUS = topEntityGeneric
 
 topEntityGA :: Clock DomReal Gated
           -> Reset DomReal Asynchronous
-          -> Signal DomDDR (Unsigned 8)
-          -> Signal DomReal (Unsigned 8, Unsigned 8)
+          -> Signal DomDDR (BitVector 8)
+          -> Signal DomReal (BitVector 8, BitVector 8)
 topEntityGA = topEntityGeneric
 
 topEntityGS :: Clock DomReal Gated
           -> Reset DomReal Synchronous
-          -> Signal DomDDR (Unsigned 8)
-          -> Signal DomReal (Unsigned 8, Unsigned 8)
+          -> Signal DomDDR (BitVector 8)
+          -> Signal DomReal (BitVector 8, BitVector 8)
 topEntityGS = topEntityGeneric
 
 
-testinputAsync  = $(listToVecTH [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16::Unsigned 8])
-testinputSync   = $(listToVecTH   [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16::Unsigned 8]) -- sync stimuliGenerator has a extra delay compared to the async one
+testinputAsync  = $(listToVecTH [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16::BitVector 8])
+testinputSync   = $(listToVecTH   [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16::BitVector 8]) -- sync stimuliGenerator has a extra delay compared to the async one
 
 
 i0 = 100
 i1 = 101
 i2 = 102
 
-testoutputAsync = (i0, i0) :> (1,2) :> (3,4):>(5,6):>(7,8):>(9,10):>(11,12):>(13,14):>((15,16)::(Unsigned 8,Unsigned 8)) :> Nil
-testoutputSync =  (i0, i0):>(i2,3):>(4,5):>(6,7):>(8,9):>(10,11):>(12,13):>((14,15)::(Unsigned 8,Unsigned 8)) :> Nil
+testoutputAsync = (i0, i0):> (1,2) :> (3,4):>(5,6):>(7,8):>(9,10):>(11,12):>(13,14):>((15,16)::(BitVector 8,BitVector 8)) :> Nil
+testoutputSync =  (i0, i0):>(i2,3):>(4,5):>(6,7):>(8,9):>(10,11):>(12,13):>((14,15)::(BitVector 8,BitVector 8)) :> Nil
 
 
 
--- tests/shouldwork/DDR/DDRout.hs
+++ tests/shouldwork/DDR/DDRout.hs
@@ -4,6 +4,8 @@ import Clash.Explicit.Testbench (ignoreFor)
 import Clash.Prelude (mux)
 import Clash.Explicit.Prelude
 import Clash.Explicit.DDR
+import Clash.Intel.DDR
+import Clash.Xilinx.DDR
 
 type DomReal = Dom "A" 2000 -- real clock domain
 type DomDDR  = Dom "A" 1000 -- fake doublespeed domain, used to model a ddr signal
@@ -16,39 +18,39 @@ The four variants defined here are all the combinations of
 
 topEntityGeneric :: Clock DomReal gated
           -> Reset DomReal synchronous
-          -> Signal DomReal (Unsigned 8,Unsigned 8)
-          -> Signal DomDDR (Unsigned 8)
+          -> Signal DomReal (BitVector 8,BitVector 8)
+          -> Signal DomDDR (BitVector 8)
 topEntityGeneric clk rst = ddrOut clk rst 0
--- topEntityGeneric = xilinxOddr
+-- topEntityGeneric = oddr
 -- topEntityGeneric = altddioOut (SSymbol @"Cyclone IV GX")
 
 
 topEntityUA :: Clock DomReal Source
           -> Reset DomReal Asynchronous
-          -> Signal DomReal (Unsigned 8,Unsigned 8)
-          -> Signal DomDDR (Unsigned 8)
+          -> Signal DomReal (BitVector 8,BitVector 8)
+          -> Signal DomDDR (BitVector 8)
 topEntityUA = topEntityGeneric
 
 topEntityUS :: Clock DomReal Source
           -> Reset DomReal Synchronous
-          -> Signal DomReal (Unsigned 8,Unsigned 8)
-          -> Signal DomDDR (Unsigned 8)
+          -> Signal DomReal (BitVector 8,BitVector 8)
+          -> Signal DomDDR (BitVector 8)
 topEntityUS = topEntityGeneric
 
 topEntityGA :: Clock DomReal Gated
           -> Reset DomReal Asynchronous
-          -> Signal DomReal (Unsigned 8,Unsigned 8)
-          -> Signal DomDDR (Unsigned 8)
+          -> Signal DomReal (BitVector 8,BitVector 8)
+          -> Signal DomDDR (BitVector 8)
 topEntityGA = topEntityGeneric
 
 topEntityGS :: Clock DomReal Gated
           -> Reset DomReal Synchronous
-          -> Signal DomReal (Unsigned 8,Unsigned 8)
-          -> Signal DomDDR (Unsigned 8)
+          -> Signal DomReal (BitVector 8,BitVector 8)
+          -> Signal DomDDR (BitVector 8)
 topEntityGS = topEntityGeneric
 
-input = ((0,1):>(2,3):>(4,5):>(6,7):>((8,9)::(Unsigned 8,Unsigned 8)) :> Nil)
-expected = (0:>0:> 0:>1:>2:>3:>4:>5:>6:>7:>8:>(9 :: Unsigned 8) :> Nil)
+input = ((0,1):>(2,3):>(4,5):>(6,7):>((8,9)::(BitVector 8,BitVector 8)) :> Nil)
+expected = (0:>0:> 0:>1:>2:>3:>4:>5:>6:>7:>8:>(9 :: BitVector 8) :> Nil)
 
 testBenchUS :: Signal DomDDR Bool
 testBenchUS = done
